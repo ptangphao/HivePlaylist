281dc26f3e228faa3bfea520e6bea74b86649408
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-07 00:29:52 -0700

    reindent controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,32 +1,32 @@
 class VideosController < ApplicationController
- include VideosHelper
+	include VideosHelper
 
-  def index
-   if playlist_started?
-  	protect_playlist
-  	@url = embed_url
-   end
-   @playlist = Video.all
-   @video = Video.new
-  end
+	def index
+		if playlist_started?
+			protect_playlist
+			@url = embed_url
+		end
+		@playlist = Video.all
+		@video = Video.new
+	end
 
-  def create
-   @video = Video.new(video_params)
-   if @video.save
-   	flash[:success] = "Successfully added #{@video.title} to the playlist."
-   else
-   	flash[:danger] = "Failed to add video to the playlist"
-   end
-   redirect_to root_path
-  end 
- 	
- 	def destroy
- 	 @video = Video.find(params[:id])
- 	 @video.destroy
- 	 redirect_to root_path
- 	end
+	def create
+		@video = Video.new(video_params)
+		if @video.save
+			flash[:success] = "Successfully added #{@video.title} to the playlist."
+		else
+			flash[:danger] = "Failed to add video to the playlist"
+		end
+		redirect_to root_path
+	end 
+	
+	def destroy
+		@video = Video.find(params[:id])
+		@video.destroy
+		redirect_to root_path
+	end
 
- 	def video_params
- 	 params.require(:video).permit(:url) 
- 	end
+	def video_params
+		params.require(:video).permit(:url) 
+	end
 end
