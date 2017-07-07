e19241d7ae7d0a678db6e89a7f4dffc88496b8ea
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-06 23:09:16 -0700

    add routes for basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	root 'videos#index'
+	post '/videos' => 'videos#create'
+	resources :videos, only: ['destroy']
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
