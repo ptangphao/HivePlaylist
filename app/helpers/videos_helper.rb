6868e7926ed6f6a1f879a132b30be2c44da6c637
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-06 23:16:55 -0700

    create extract_id method to grab youtube video ID from url

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -3,5 +3,10 @@ module VideosHelper
 	PLAYLIST_OPTION = "?playlist="
 	PLAYER_OPTIONS = "&autoplay=1&controls=1&&showinfo=1&loop=1"
 	YOUTUBE_REGEX = /youtu(?:.*\/v\/|.*v\=|\.be\/)([A-Za-z0-9_\-]{11})/
-	
+
+	def extract_id(url)
+		basic_url = url.match(YOUTUBE_REGEX).to_s
+		youtube_id = basic_url[-11..-1]
+		return youtube_id
+	end
 end
